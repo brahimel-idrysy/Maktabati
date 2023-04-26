7648e23d18e3418c780614e3974360ca565eb205
Brahim El-idrysy <elidrysybrahim23@gamil.com>
2023-04-26 15:57:46 +0100

    Library app

---
+++ lib/frentend/home_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class homePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/frentend/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class splashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(253, 6, 164, 61),
+      body: SafeArea(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [SvgPicture.asset('images/logo.svg')]),
+      ),
+    );
+  }
+}
